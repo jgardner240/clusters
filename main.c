d1653a5a17abfd85a4b2e568874c15680d5c89e8
Jason Gardner <jason.the.gorilla@gmail.com>
2016-07-07 20:14:00 -0700

    Adding final solution.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define SENSOR_WIDTH  20
 #define SENSOR_HEIGHT 20
 
-#define SEARCHED	0xFF
+#define SEARCHED	-1
 typedef union
 {
 	short pixel[SENSOR_HEIGHT * SENSOR_WIDTH];
@@ -19,10 +19,10 @@ short array[SENSOR_HEIGHT][SENSOR_WIDTH] = {
 		{1,1,1,0,0,1,1,1,1,1,1,0,0,0,1,1,1,0,1,1},
 		{1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -33,6 +33,30 @@ short array[SENSOR_HEIGHT][SENSOR_WIDTH] = {
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 };
 
+int searchNeighbors(int row, int col)
+{
+	int count = 1;
+	int rowMin, rowMax, colMin, colMax;
+
+	rowMin = (row > 0) ? (row - 1) : 0;
+	colMin = (col > 0) ? (col - 1) : 0;
+	rowMax = (row < (SENSOR_HEIGHT - 1)) ? (row + 1) : (SENSOR_HEIGHT - 1);
+	colMax = (col < (SENSOR_WIDTH - 1)) ? (col + 1) : (SENSOR_WIDTH - 1);
+
+	array[row][col] = SEARCHED;
+
+	for (int x = rowMin; x <= rowMax; x++) {
+		for (int y = colMin; y <= colMax; y++) {
+			if (array[x][y] == 0) {
+				count += searchNeighbors(x,y);
+			}
+			else
+				array[x][y] = SEARCHED;
+		}
+	}
+	return count;
+}
+
 int main(void)
 {
 	int i, count;
@@ -42,15 +66,13 @@ int main(void)
 	for (int row=0; row<SENSOR_HEIGHT; row++) {
 		for (int col=0; col<SENSOR_WIDTH; col++) {
 			if (array[row][col] == 0) {
-				count++;
-				array[row][col] = SEARCHED;
+				count += searchNeighbors(row,col);
+				printf("Cluster %d, count %d\n", currentClusterCount++, count);
+				count = 0;
 			}
 		}
 	}
-	printf("Count = %d\n", count);
+
+	return 0;
 }
 
-int searchNeighbors(int row, int col)
-{
-
-}
